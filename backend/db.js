ef20a15a11f4c09ebe95eed86ea11977fc1e1cf2
anilkumar1 <anilkpaital1@gmail.com>
2026-03-29 12:02:16 +0530

    switch to postgres

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
-const db = mysql.createPool({
+const { Pool } = require("pg");
+
+const db = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
   },
-
-  waitForConnections: true,
-  connectionLimit: 10,
 });
+
+module.exports = db;
